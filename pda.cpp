6138c2c5acf482493d4c3794155f1ee0bca124b1
貝時計 KAITOKEI <m21148@g.metro-cit.ac.jp>
2024-12-04 18:34:26 +0900

    add: read_file func

--- pda.cpp
+++ pda.cpp
@@ -9,6 +9,8 @@ using namespace std;
 // グラフデータの入力フォーマット
 /*
 N (グラフの頂点の数)
+q0 (初期状態)
+z0 (初期スタック記号)
 0,0:a:Z/ZA
 (input):(from):(to):(stackのbackの記号)/(pushする記号)  <- pushの場合
 (input):(from):(to):(stackのbackの記号)/(e)  <- popの場合
@@ -19,30 +21,34 @@ N (グラフの頂点の数)
 Vertex構造体を作るよりも
 Navigation構造体を使い
 現在の遷移状態をポインタとして保持した方が、柔軟性が高いのではないか？
-ポインタを持つ主体としてのPDAが世界線に合わせて分裂していく？
+ポインタを持つ主体としてのPDAが、進行に伴い分裂していく？
 */
 
 // 状態遷移函数
 struct Transition
 {
-    string input; // 入力
-    string str;   // スタック記号
     string to;    // 遷移先
+    string r_str; // スタックで読み取る記号
+    string p_str; // スタックに積む記号
 };
 
 struct PDA
 {
     // 状態集合Qは、idとしてのみ保持する
-    string sigma;                       // 入力記号集合
     vector<string> ganma;               // スタック記号集合
-    map<string, set<Transition>> delta; // 状態遷移函数の集合
+    map<string, set<Transition>> delta; // 状態遷移函数の集合(入力記号:それに関する状態遷移函数)
     string q0;                          // 初期状態
     string z0;                          // 最初のスタックに記録されるスタック記号
 
-    PDA(string _sigma, string _q0, string _z0)
+    PDA(int _q0, string _z0)
     {
-        sigma = _sigma;
         q0 = _q0;
         z0 = _z0;
+    }
+
+    // 遷移先を追加する
+    void add_transition(string key, Transition transition)
+    {
+        delta[key].insert(transition);
     }
 };
--- utils.cpp
+++ utils.cpp
@@ -1,13 +1,83 @@
 #pragma once
 #include <vector>
 #include <fstream>
+#include <sstream>
+#include <utility>
 #include "pda.cpp";
 
 using namespace std;
 
-// ファイルからグラフデータを取得する
-vector<PDA> read_graph_from_file()
+// slashで分割されたデータを分解する
+pair<string, string> get_slash_divided_data(string input)
 {
+    string l = "", r = "";
+    bool slash = false;
+    for (int i = 0; i < input.size(); i++)
+    {
+        if (input[i] == '/')
+        {
+            slash = true;
+        }
+        else
+        {
+            if (slash)
+            {
+                r += input[i];
+            }
+            else
+            {
+                l += input[i];
+            }
+        }
+    }
+}
 
-    int N;
+// ファイルからグラフデータを取得する
+vector<PDA> read_graph_from_file(string file_path)
+{
+    ifstream file(file_path); // グラフデータのファイル
+    string line;              // 入力行
+
+    int N;     // 状態集合の数
+    int q0;    // 初期状態のid
+    string z0; // 初期スタック記号
+
+    // 状態集合の数を取得する
+    getline(file, line);
+    N = stoi(line);
+
+    // 初期状態を取得
+    getline(file, line);
+    q0 = stoi(line);
+
+    // 初期スタック記号を取得
+    getline(file, line);
+    z0 = line;
+
+    PDA pda(q0, z0); // プッシュダウンオートマトンのインスタンス
+
+    while (getline(file, line))
+    {
+        stringstream ss(line);
+        vector<string> vals;
+        string s;
+
+        // コロンで入力文字列を分割する
+        while (getline(ss, s, ':'))
+        {
+            vals.push_back(s);
+        }
+
+        string input = vals[0]; // 入力記号
+        string from = vals[1];  // 遷移元
+        string to = vals[2];    // 遷移先
+
+        pair<string, string> slashed = get_slash_divided_data(vals[3]);
+
+        // スタックに関する記号
+        string r_str = slashed.first, p_str = slashed.second;
+
+        // 遷移先を追加
+        pda.add_transition(input, Transition{to, r_str, p_str});
+    }
 }
