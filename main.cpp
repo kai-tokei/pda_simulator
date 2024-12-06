b4fb7108972ee50adc0d65eebe2c976107626018
貝時計 KAITOKEI <m21148@g.metro-cit.ac.jp>
2024-12-06 19:18:34 +0900

    add: command line argument

--- main.cpp
+++ main.cpp
@@ -4,13 +4,22 @@
 
 using namespace std;
 
-int main()
+int main(int argc, char *argv[])
 {
+    if (argc < 3)
+    {
+        cerr << "Error: Not enough arguments" << endl;
+        return 0;
+    }
+
     // プッシュダウンオートマトンを読み込み
     vector<PDA> pdas;
-    pdas.push_back(read_graph_from_file("./sub.txt"));
+    pdas.push_back(read_graph_from_file(argv[1]));
 
-    cout << pdas[0].to_str() << endl;
+    // 入力列を読み込み
+    string input = argv[2];
+
+    // オートマトンを実行
 
     return 0;
 }
